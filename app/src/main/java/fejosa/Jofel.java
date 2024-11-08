bc467d2702b46a365baa44f24c26bbf3d10f4151
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2024-11-07 23:49:45 -0300

    utilizando o gradle como builder

---
+++ app/src/main/java/fejosa/Jofel.java
@@ -0,0 +1,7 @@
+package fejosa;
+
+public class Jofel {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ app/src/test/java/fejosa/JofelTest.java
@@ -0,0 +1,4 @@
+package fejosa;
+
+public class JofelTest {
+}
