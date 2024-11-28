f15c6a993e144205b69aa17fb72ffc8ddbe486c3
Felipe Piassa <felipepiassa1802@gmail.com>
2024-11-28 18:49:44 -0300

    .

--- app/src/main/java/fejosa/Jofel.java
+++ app/src/main/java/fejosa/Jofel.java
@@ -2,6 +2,6 @@ package fejosa;
 
 public class Jofel {
     public static void main(String[] args) {
-        System.out.println("Hello World ");
+        System.out.println("Hello World fd");
     }
 }
