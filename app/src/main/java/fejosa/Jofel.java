5a295698583ad7f59ad00e9f16adc1a5ca0f6943
Felipe Piassa <felipepiassa1802@gmail.com>
2025-01-07 22:05:39 -0300

    mostra lista de tokens e arvore

--- app/src/main/java/fejosa/Jofel.java
+++ app/src/main/java/fejosa/Jofel.java
@@ -1,6 +1,10 @@
 package fejosa;
 
 import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.util.List;
+
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.tree.ParseTree;
 
@@ -8,25 +12,26 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Jofel {
     public static void main(String[] args) {
-        String teste = "i32 coiso = 1234;\n"
-                + "f32 coiso6 = 123.456;\n"
-                + "// blablablablabla\n"
-                + "string coiso2 = \"hello world\";\n"
-                + "u8 coiso3 = 'a';\n"
-                + "fn main() -> void {\n"
-                + "    if a == 10 {\n"
-                + "    } else if b == 11 {\n"
-                + "    } else {\n"
-                + "    }\n"
-                + "for i in 0..10 {\n"
-                + "}\n"
-                + "struct Bunda {\n"
-                + "    i32 a,\n"
-                + "    u32 b,\n"
-                + "}\n"
-                + "              ^";
-
         /*
+         * String teste = "i32 coiso = 1234;\n"
+         * + "f32 coiso6 = 123.456;\n"
+         * + "// blablablablabla\n"
+         * + "string coiso2 = \"hello world\";\n"
+         * + "u8 coiso3 = 'a';\n"
+         * + "fn main() -> void {\n"
+         * + "    if a == 10 {\n"
+         * + "    } else if b == 11 {\n"
+         * + "    } else {\n"
+         * + "    }\n"
+         * + "for i in 0..10 {\n"
+         * + "}\n"
+         * + "struct Bunda {\n"
+         * + "    i32 a,\n"
+         * + "    u32 b,\n"
+         * + "}\n"
+         * + "              ^";
+         * 
+         * 
          * Lexer lexer = new Lexer(teste);
          * Token token = lexer.leToken();
          * while (token.tipo != TipoToken.EOF) {
@@ -41,15 +46,25 @@ public class Jofel {
         String input = "x+2;";
 
         // Criando o Lexer
-
         GramaticaLexer lexer = new GramaticaLexer(CharStreams.fromString(input));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // Força a criação do fluxo de tokens e imprimi tokens encontrados
+        tokens.fill();
+        List<Token> allTokens = tokens.getTokens();
+
+        System.out.println("--- TOKENS ENCONTRADOS ---");
+        for (Token token : allTokens)
+            System.out.println(
+                    "Lexema: " + token.getText() + " Token: " +
+                            lexer.getVocabulary().getSymbolicName(token.getType()));
 
         // Criando o Parser
         GramaticaParser parser = new GramaticaParser(tokens);
         ParseTree tree = parser.prog();
 
         // Imprimindo a árvore de parsing
+        System.out.println("\n--- ÁRVORE SINTÁTICA ---");
         System.out.println(tree.toStringTree(parser));
 
     }
