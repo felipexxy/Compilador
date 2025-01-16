9f2e191668291bc30444ad2669cf25005ec102b2
Felipe Piassa <felipepiassa1802@gmail.com>
2025-01-15 22:15:56 -0300

    Add visualização da árvore sintática

--- app/src/main/java/fejosa/Jofel.java
+++ app/src/main/java/fejosa/Jofel.java
@@ -1,15 +1,17 @@
 package fejosa;
 
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.gui.TreeViewer;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
-
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.tree.ParseTree;
+import java.util.Arrays;
 
 public class Jofel {
     public static void main(String[] args) {
@@ -32,15 +34,16 @@ public class Jofel {
         GramaticaLexer lexer = new GramaticaLexer(CharStreams.fromString(teste));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        // Força a criação do fluxo de tokens e imprimi tokens encontrados
+        // Força a criação do fluxo de tokens e imprime tokens encontrados
         tokens.fill();
         List<Token> allTokens = tokens.getTokens();
 
         System.out.println("--- TOKENS ENCONTRADOS ---");
-        for (Token token : allTokens)
+        for (Token token : allTokens) {
             System.out.println(
                     "Lexema: " + token.getText() + " Token: " +
                             lexer.getVocabulary().getSymbolicName(token.getType()));
+        }
 
         // Criando o Parser
         GramaticaParser parser = new GramaticaParser(tokens);
@@ -50,5 +53,15 @@ public class Jofel {
         System.out.println("\n--- ÁRVORE SINTÁTICA ---");
         System.out.println(tree.toStringTree(parser));
 
+        // Visualização gráfica da árvore
+        JFrame frame = new JFrame("Árvore Sintática");
+        JPanel panel = new JPanel();
+        TreeViewer viewer = new TreeViewer(Arrays.asList(parser.getRuleNames()), tree);
+        viewer.setScale(1.5); // Ajuste da escala da árvore
+        panel.add(viewer);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
     }
 }
